717c9de8edf4e0dbe0d90950f08b6a9746040b1a
Jyothi <lakshmijyo2003@gmail.com>
2023-08-04 16:11:09 +0530

    new

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React from start
         </a>
       </header>
     </div>
